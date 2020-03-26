7c0b44635529831f4623ba7b1c7dd8b2b5ac05fe
mohitkumar45 <61778852+mohitkumar45@users.noreply.github.com>
2020-03-26 12:47:38 +0530

    how to read and write data

---
+++ Exelsheet/src/ReadWrite/ReadCellExample.java
@@ -0,0 +1,39 @@
+package ReadWrite;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class ReadCellExample {
+	public static void main(String[] args) {
+		ReadCellExample rc = new ReadCellExample();
+
+		String vOutput = rc.ReadCellData(1, 0);
+		System.out.println(vOutput);
+	}
+
+	public String ReadCellData(int vRow, int vColumn) {
+		String value = null;
+		Workbook wb = null;
+		try {
+
+			FileInputStream fis = new FileInputStream("C:\\Users\\91975\\Documents\\student.xls");
+
+			wb = new HSSFWorkbook(fis);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		Sheet sheet = wb.getSheetAt(0);
+		Row row = sheet.getRow(vRow);
+		Cell cell = row.getCell(vColumn);
+		value = cell.getStringCellValue();
+		return value;
+	}
+}
